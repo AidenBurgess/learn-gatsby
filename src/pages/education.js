66ca328c5050dba3c0f0d860f2e877b2d945d2ec
Aiden Burgess <aiden9874@gmail.com>
2020-12-06 22:50:55 +1300

    change education info

--- src/pages/education.js
+++ src/pages/education.js
@@ -9,7 +9,7 @@ export default function Education() {
 
       <div className="container">
         <h1>Education Info</h1>
-        <p>Reprehenderit Lorem anim ex tempor duis Lorem in. Adipisicing magna deserunt et exercitation sint aliqua cillum ullamco do cillum excepteur est do. Magna deserunt quis tempor laborum cillum excepteur cupidatat eiusmod esse labore cupidatat. Est officia Lorem consequat ad esse esse cupidatat amet magna ullamco amet non. Ipsum cillum aute enim sint proident ut sit ullamco voluptate ut commodo proident tempor.</p>
+        <p>I am currently attending the University of Auckland</p>
         <h1>My Interests</h1>
         <p>Deserunt ex fugiat est excepteur velit veniam sint cupidatat. Minim fugiat ut quis commodo ea quis ea ut quis officia magna dolor esse. Irure nostrud esse nisi ad mollit in duis voluptate anim. Veniam pariatur quis mollit pariatur. Ex enim consectetur quis irure sint fugiat proident ut reprehenderit id aliqua cillum.</p>
       </div>
