c4d8e743bcbc55c77a07fde8957762e0a518529c
Aiden Burgess <aiden9874@gmail.com>
2020-12-06 21:05:40 +1300

    Add all pages

--- my-portfolio/src/pages/education.js
+++ my-portfolio/src/pages/education.js
@@ -2,13 +2,13 @@ import React from "react"
 import Header from "../components/header"
 import "../styles.css"
 
-export default function Home() {
+export default function Education() {
   return (
     <div>
       <Header/>
 
       <div className="container">
-        <h1>About Me</h1>
+        <h1>Education Info</h1>
         <p>Reprehenderit Lorem anim ex tempor duis Lorem in. Adipisicing magna deserunt et exercitation sint aliqua cillum ullamco do cillum excepteur est do. Magna deserunt quis tempor laborum cillum excepteur cupidatat eiusmod esse labore cupidatat. Est officia Lorem consequat ad esse esse cupidatat amet magna ullamco amet non. Ipsum cillum aute enim sint proident ut sit ullamco voluptate ut commodo proident tempor.</p>
         <h1>My Interests</h1>
         <p>Deserunt ex fugiat est excepteur velit veniam sint cupidatat. Minim fugiat ut quis commodo ea quis ea ut quis officia magna dolor esse. Irure nostrud esse nisi ad mollit in duis voluptate anim. Veniam pariatur quis mollit pariatur. Ex enim consectetur quis irure sint fugiat proident ut reprehenderit id aliqua cillum.</p>
